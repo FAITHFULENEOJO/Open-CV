09021aabb4810fe8ead883fe975244cc8e6d5e3e
FAITHFULENEOJO <147441465+FAITHFULENEOJO@users.noreply.github.com>
2024-07-17 16:19:03 +0100

    Update main.py

--- Open CV/main.py
+++ Open CV/main.py
@@ -2,7 +2,7 @@ import cv2
 
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 image = cv2.imread('smiles.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRY)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Gray", gray)
 cv2.waitKey()
 
